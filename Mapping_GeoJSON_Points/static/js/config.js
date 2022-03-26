79a79e7369d815151249f56e57d90360cfcc5363
Hemant Kiri <hemantkiri@Hemants-MacBook-Pro.local>
2022-03-26 10:37:14 -0700

    Mapping_Earthquakes

---
+++ Mapping_GeoJSON_Points/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiaGVtYW50a2lyaSIsImEiOiJjbDExaGhxZmIwMXF2M2lxdXk2cmFiYjV2In0.HkF-JqYFom7-5P1y-wfmnQ";
--- Simple_Leaflet_Map/static/js/config.js
+++ Simple_Leaflet_Map/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiaGVtYW50a2lyaSIsImEiOiJjbDExaGhxZmIwMXF2M2lxdXk2cmFiYjV2In0.HkF-JqYFom7-5P1y-wfmnQ";
+const API_KEY = "API KEY";
